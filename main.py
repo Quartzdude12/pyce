a64b0a4cd21b0498b36dc43bdeaad12cdb99c195
Quartzdude12 <carlitoun@hotmail.com>
2025-05-29 22:50:08 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#made by Quartzdude12 aka CAA or Betrayed_
+import math
+import random
+import os
+
+def main():
+  if os.name == 'nt':
+    os.system('cls')
+    # For Linux/Mac
+  else:
+      os.system('clear')
+  sides = int(input("Sides: "))
+  amount = int(input("How many times?: "))
+  for i in range(amount):
+    print(random.randint(1, sides))
+  time.sleep(4)
+
+main()
